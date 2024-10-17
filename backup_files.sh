02e9a65c61eaa40e265a09ef2eddca50c4cfd88d
Rafael Fernandes <145208611+rafaeltorrinhas@users.noreply.github.com>
2024-10-17 15:35:41 +0100

    Update backup_files.sh

--- backup_files.sh
+++ backup_files.sh
@@ -6,7 +6,7 @@ if [[ $# -lt 2 || $# -gt 3 ]]; then
   exit 1
 fi
 
-# Modo de checking
+# Modo de checking (por default copia os ficheiros)
 CHECK_MODE=""
 
 # Verifica se a opção -c foi fornecida
@@ -27,21 +27,25 @@ if [[ ! -d "$SRC_DIR" ]]; then
   exit 1
 fi
 
-# Cria o diretório de destino caso este não exista
-
+# Cria o diretório de destino se não existir
 if [[ ! -d "$BACKUP_DIR" ]]; then
   echo "mkdir -p '$BACKUP_DIR'"
+  # Se não estiver em checking cria a pasta
   if [[ $CHECK_MODE != "-c" ]]; then
     mkdir -p "$BACKUP_DIR" || { echo "Erro ao criar o diretório de destino"; exit 1; }
   fi
 fi
+
 # Percorre todos os arquivos no diretório de origem
 for FILE in "$SRC_DIR"/*; do
+  # Verifica se é um ficheiro (não diretório)
   if [[ -f "$FILE" ]]; then
+    # Guarda o caminho do ficheiro
     BACKUP_FILE="$BACKUP_DIR/$(basename "$FILE")"
-
+    # Verifica se o ficheiro já existe ou, se exstir, a data de modificação.
     if [[ ! -f "$BACKUP_FILE" || "$FILE" -nt "$BACKUP_FILE" ]]; then
       echo "cp '$FILE' '$BACKUP_FILE'"
+      # Se não estiver em checking cria cópias dos ficheiros
       if [[ "$CHECK_MODE" != "-c" ]]; then
         cp "$FILE" "$BACKUP_FILE" || { echo "Erro ao copiar '$FILE'"; exit 1; }
       fi
